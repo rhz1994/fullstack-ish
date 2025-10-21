2b605c5f1358118ea32d9374cd8d52f9dc581069
Rasmus Hanzén <rhz1994@gmail.com>
2025-10-21 13:45:52 +0200

    Added postgres

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,29 @@
 const express = require("express"),
   path = require("path");
-
 const app = express();
-
 const port = process.env.PORT || 3000;
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "World" });
-});
+dotenv.config();
 
 app.use(express.static(path.join(path.resolve(), "dist")));
 
 app.listen(port, () => {
   console.log(`Backend är igång på port: ${port}`);
   console.log("Path", path.resolve());
+});
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+app.get("/api", async (_request, response) => {
+  const { rows } = await client.query("SELECT * FROM cities WHERE name = $1", [
+    "Stockholm",
+  ]);
+
+  response.send(rows);
 });
