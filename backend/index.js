58e2d5fa9d25ecc9bc5b8c91f37c3585da569bfe
Rasmus Hanzén <rhz1994@gmail.com>
2025-10-21 10:04:30 +0200

    First commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/api", (_request, response) => {
+  response.send({ hello: "World" });
+});
+
+app.use(express.static(path.join(path.resolve(), "dist")));
+
+app.listen(port, () => {
+  console.log(`Backend är igång på port: ${port}`);
+});
